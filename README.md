7503ae2d46855ae6a5514fa9709069c50ab2098e
sookyeongyeom <sookyeong.dev@gmail.com>
2022-08-03 19:58:50 +0900

    :books: p250

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 - [x] 가장 흔한 방식, 일반 CSS
 - [x] Sass 사용하기
-- [ ] CSS Module
-- [ ] styled-components
-- [ ] 정리
+- [x] CSS Module
+- [x] styled-components
+- [x] 정리
--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import SassComponent from './SassComponent';
 import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 function App() {
   return (
     <>
       <SassComponent />
       <CSSModule />
+      <StyledComponent />
     </>
   );
 }
